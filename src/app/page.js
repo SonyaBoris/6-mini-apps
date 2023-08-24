627ea49d5496c226925ffd7fb820a962f04cf9fd
SonyaBoris <sweetboris13524@gmail.com>
2023-08-24 15:16:11 +0400

    стили

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.container}>
-      <h1>6 мини приложений на React</h1>
+      <h1 className={styles.title}>6 мини приложений на React</h1>
       
     </main>
   )
