51ecef0d3fd2a9cf0b53b0b6109757e8eb7ca790
SonyaBoris <sweetboris13524@gmail.com>
2023-08-28 10:31:37 +0400

    верстка конвертера валют

--- src/app/converter/Block.jsx
+++ src/app/converter/Block.jsx
@@ -20,7 +20,7 @@ const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => {
         <li className="arrow">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <g transform="rotate(90 12 12)">
-              <path fill="none" stroke="currentColor" stroke-width="2" d="m7 2l10 10L7 22" />
+              <path fill="none" stroke="currentColor" strokeWidth="2" d="m7 2l10 10L7 22" />
             </g>
           </svg>
         </li>
--- src/app/converter/page.jsx
+++ src/app/converter/page.jsx
@@ -8,10 +8,42 @@ import styles from './page.module.css';
 
 
 const Converter = () => {
+
+  const [fromCurrency, setFromCurrency] = React.useState('RUB');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+
+  const [rates, setRates] = React.useState({});
+
+  // React.useEffect(() => {
+  //   fetch('https://www.cbr-xml-daily.ru/latest.js')
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setRates(json.rates)
+
+  //     }).catch(err => {
+  //       console.warn(err)
+  //       alert('не удалось получить информацию')
+  //     })
+
+  // });
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency]
+    const result = price * rates[toCurrency]
+    setFromPrice(value)
+    setToPrice(result)
+  };
+  const onChangeToPrice = (value) => {
+    setToPrice(value)
+  };
+
   return (
     <main className={styles.container}>
-      <Block value={0} currency='RUB' />
-      <Block value={0} currency='USD' />
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice} />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice} />
+
 
     </main>
   );
